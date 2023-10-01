d98dcdae71dcb4bcb9d000694307b48bb0b77edc
ramboe <cle.kruse@hotmail.com>
2023-10-01 11:37:17 +0200

    fixed LoginForm.razor +  cleanups

--- Ramboe.Logging.Blazor/Client/Pages/Logs.razor.cs
+++ Ramboe.Logging.Blazor/Client/Pages/Logs.razor.cs
@@ -12,8 +12,6 @@ public partial class Logs
 
     [Parameter] public string Level { get; set; }
 
-    // [Parameter] public string Id { get; set; }
-
     private LogModel[] LogsFromApi { get; set; } =
     {
     };
@@ -59,13 +57,10 @@ public partial class Logs
         }
     }
 
-    ///////////
-    ///
     private string _searchString;
 
     private bool _sortNameByLength;
 
-    // private List<string> _events = new();
 
     // custom sort by name length
     private Func<LogModel, object> _sortBy => x => {
@@ -105,34 +100,6 @@ public partial class Logs
 
     public Dictionary<string, string> Colors { get; set; } = new();
 
-    // private Func<LogModel, string> _cellStyleFunc => (x) => {
-    //     if (LogsFromApi.Count(log => log.Id == x.Id) <= 1)
-    //     {
-    //         return "";
-    //     }
-    //
-    //     double percentage = 15;
-    //
-    //     if (Colors.ContainsKey(x.Id))
-    //     {
-    //         BackgroundColorForDoubles = Colors[x.Id];
-    //     }
-    //     else
-    //     {
-    //         // BackgroundColorForDoubles = ColorHelper.DarkenHexColor(BackgroundColorForDoubles, percentage);
-    //         // BackgroundColorForDoubles = ColorHelper.GenerateRandomColor("#258cfb");
-    //         // BackgroundColorForDoubles = ColorHelper.GenerateRandomColor("#eab676");
-    //         BackgroundColorForDoubles = ColorHelper.GenerateSlightlyDifferentShade("#BD76E9");
-    //
-    //         // BackgroundColorForDoubles = ColorHelper.GenerateRandomColor("#6099EB");
-    //         // BackgroundColorForDoubles = ColorHelper.GenerateRandomColor("#1a66d2");
-    //         // BackgroundColorForDoubles = ColorHelper.GenerateRandomColor("#C6DBE9");
-    //         Colors.Add(x.Id, BackgroundColorForDoubles);
-    //     }
-    //
-    //     return $"background-color: {BackgroundColorForDoubles}; color: #fff;";
-    // };
-
     private Func<LogModel, int, string> _rowStyleFunc => (x, i) => {
         if (LogsFromApi.Count(log => log.Id == x.Id) <= 1)
         {
@@ -147,15 +114,8 @@ public partial class Logs
         }
         else
         {
-            // BackgroundColorForDoubles = ColorHelper.DarkenHexColor(BackgroundColorForDoubles, percentage);
-            // BackgroundColorForDoubles = ColorHelper.GenerateRandomColor("#258cfb");
-            // BackgroundColorForDoubles = ColorHelper.GenerateRandomColor("#eab676");
-            // BackgroundColorForDoubles = ColorHelper.GenerateSlightlyDifferentShade("#BD76E9", 0.15);
             BackgroundColorForDoubles = ColorHelper.GenerateSlightlyDifferentShade("#ac53e3", 0.3);
 
-            // BackgroundColorForDoubles = ColorHelper.GenerateRandomColor("#6099EB");
-            // BackgroundColorForDoubles = ColorHelper.GenerateRandomColor("#1a66d2");
-            // BackgroundColorForDoubles = ColorHelper.GenerateRandomColor("#C6DBE9");
             Colors.Add(x.Id, BackgroundColorForDoubles);
         }
 
